02a2fdf51b91986d26193ecb58ef76646d794155
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-04-22 21:49:48 +0530

    Update 10-divide-one-number-by-another-with-javascript.js

--- basic_javaScript/10-divide-one-number-by-another-with-javascript.js
+++ basic_javaScript/10-divide-one-number-by-another-with-javascript.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-number-by-another-with-javascript
-*/
+
 
 /* =============== First Solution =============== */
 
