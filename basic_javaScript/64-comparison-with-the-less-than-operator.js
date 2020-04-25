f5e1e17bf3cb689d14c9fd3216e10b83fd3f68e9
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-04-25 20:02:03 +0530

    Update 64-comparison-with-the-less-than-operator.js

--- basic_javaScript/64-comparison-with-the-less-than-operator.js
+++ basic_javaScript/64-comparison-with-the-less-than-operator.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-less-than-operator
-*/
+
 
 /* =============== First Solution =============== */
 
