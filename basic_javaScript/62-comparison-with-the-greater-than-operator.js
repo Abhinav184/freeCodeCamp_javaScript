905fe98a90e285e2f1855dbf1ae6ab1d06e6153f
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-04-25 20:02:23 +0530

    Update 62-comparison-with-the-greater-than-operator.js

--- basic_javaScript/62-comparison-with-the-greater-than-operator.js
+++ basic_javaScript/62-comparison-with-the-greater-than-operator.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-greater-than-operator
-*/
 
 /* =============== First Solution =============== */
 
