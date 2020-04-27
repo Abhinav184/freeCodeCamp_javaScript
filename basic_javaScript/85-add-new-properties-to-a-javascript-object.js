e3292405d5bd184069ff39c367e1c14efded6763
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-04-27 21:36:58 +0530

    Update 85-add-new-properties-to-a-javascript-object.js

--- basic_javaScript/85-add-new-properties-to-a-javascript-object.js
+++ basic_javaScript/85-add-new-properties-to-a-javascript-object.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-new-properties-to-a-javascript-object
-*/
+
 
 /* =============== First Solution =============== */
 
