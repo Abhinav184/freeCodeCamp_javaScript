0080b5134f9c17e6fded8f824fce9ad7bafc1108
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-04-27 21:37:49 +0530

    Update 80-build-javascript-objects.js

--- basic_javaScript/80-build-javascript-objects.js
+++ basic_javaScript/80-build-javascript-objects.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/build-javascript-objects
-*/
 
 /* =============== First Solution =============== */
 
