5db49a241fff9b3ffc315ec31f37d91e99acddac
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-01 22:43:26 +0530

    Update 32-use-bracket-notation-to-find-the-nth-character-in-a-string.js

--- basic_javaScript/32-use-bracket-notation-to-find-the-nth-character-in-a-string.js
+++ basic_javaScript/32-use-bracket-notation-to-find-the-nth-character-in-a-string.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-nth-character-in-a-string
-*/
+
 
 /* =============== First Solution =============== */
 
