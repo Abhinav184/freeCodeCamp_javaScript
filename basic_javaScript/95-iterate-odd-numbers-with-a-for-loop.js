5aae30321a6909c915b5d106d0190d846245cab5
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-03 22:55:59 +0530

    Update 95-iterate-odd-numbers-with-a-for-loop.js

--- basic_javaScript/95-iterate-odd-numbers-with-a-for-loop.js
+++ basic_javaScript/95-iterate-odd-numbers-with-a-for-loop.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-odd-numbers-with-a-for-loop
-*/
+
 
 /* =============== First Solution =============== */
 
