5ca67cfffff64a8c2be4aefc63f7c1bf03369199
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-03 22:56:19 +0530

    Update 93-iterate-with-javascript-while-loops.js

--- basic_javaScript/93-iterate-with-javascript-while-loops.js
+++ basic_javaScript/93-iterate-with-javascript-while-loops.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-while-loops
-*/
 
 /* =============== First Solution =============== */
 
