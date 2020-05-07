1a0f8deadb3835832cf25bdd77c186a05b16f8c7
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-07 23:06:13 +0530

    Update 3-storing-values-with-the-assignment-operator.js

--- basic_javaScript/3-storing-values-with-the-assignment-operator.js
+++ basic_javaScript/3-storing-values-with-the-assignment-operator.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator
-*/
 
 /* =============== First Solution =============== */
 
