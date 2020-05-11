a7fde1da32637a2943d9d75b9deb65d426edbbe4
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-11 23:42:29 +0530

    Update 100-replace-loops-using-recursion.js

--- basic_javaScript/100-replace-loops-using-recursion.js
+++ basic_javaScript/100-replace-loops-using-recursion.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/replace-loops-using-recursion
-*/
+
 
 /* =============== First Solution =============== */
 
