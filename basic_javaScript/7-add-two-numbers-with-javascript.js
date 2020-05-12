22883bcd9db6a4e370aa8656ab9f87de46434bab
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-12 21:29:54 +0530

    Update 7-add-two-numbers-with-javascript.js

--- basic_javaScript/7-add-two-numbers-with-javascript.js
+++ basic_javaScript/7-add-two-numbers-with-javascript.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-two-numbers-with-javascript
-*/
+
 
 /* =============== First Solution =============== */
 
