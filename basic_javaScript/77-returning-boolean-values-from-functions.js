6baae7fc128f1279c3e58d0af0ea57fb987cff95
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-13 22:56:40 +0530

    Update 77-returning-boolean-values-from-functions.js

--- basic_javaScript/77-returning-boolean-values-from-functions.js
+++ basic_javaScript/77-returning-boolean-values-from-functions.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/returning-boolean-values-from-functions
-*/
 
 /* =============== First Solution =============== */
 
