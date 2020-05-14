9eb67a7397de826a9a8d4265138c5edc302d1b87
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-14 23:58:26 +0530

    Update 78-return-early-pattern-for-functions.js

--- basic_javaScript/78-return-early-pattern-for-functions.js
+++ basic_javaScript/78-return-early-pattern-for-functions.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/return-early-pattern-for-functions
-*/
+
 
 /* =============== First Solution =============== */
 
