7bef874f0b739b8fea125984d4d91daf961e5aab
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-14 23:59:45 +0530

    Update 72-golf-code.js

--- basic_javaScript/72-golf-code.js
+++ basic_javaScript/72-golf-code.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/golf-code
-*/
+
 
 /* =============== First Solution =============== */
 
