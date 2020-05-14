be4b6fdde4f3ff53c767b4733ea57a14726dc56f
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-14 23:59:29 +0530

    Update 73-selecting-from-many-options-with-switch-statements.js

--- basic_javaScript/73-selecting-from-many-options-with-switch-statements.js
+++ basic_javaScript/73-selecting-from-many-options-with-switch-statements.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/selecting-from-many-options-with-switch-statements
-*/
 
 /* =============== First Solution =============== */
 
