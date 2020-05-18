922c1a9832425e20b3cbecf83930d5cbdb8d8273
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-18 21:42:57 +0530

    Update 106-use-the-parseint-function-with-a-radix.js

--- basic_javaScript/106-use-the-parseint-function-with-a-radix.js
+++ basic_javaScript/106-use-the-parseint-function-with-a-radix.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function-with-a-radix
-*/
 
 /* =============== First Solution =============== */
 
