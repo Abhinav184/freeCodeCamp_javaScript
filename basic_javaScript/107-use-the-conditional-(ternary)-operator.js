2a6f66d78395c5b688c75296a3a76715197895a0
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-18 21:43:09 +0530

    Update 107-use-the-conditional-(ternary)-operator.js

--- basic_javaScript/107-use-the-conditional-(ternary)-operator.js
+++ basic_javaScript/107-use-the-conditional-(ternary)-operator.js
@@ -1,6 +1,3 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-conditional-ternary-operator
-*/
 
 /* =============== First Solution =============== */
 
