b0f9c2a50114495a55d9563ff26fc34627edfe2c
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-18 21:42:45 +0530

    Update 105-use-the-parseint-function.js

--- basic_javaScript/105-use-the-parseint-function.js
+++ basic_javaScript/105-use-the-parseint-function.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function
-*/
+
 
 /* =============== First Solution =============== */
 
