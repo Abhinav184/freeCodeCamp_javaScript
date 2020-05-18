8e27fbe2e6b4fbcfda8c9befe7cdb069d38a88e7
Kumar Abhinav Jha <32488220+Abhinav184@users.noreply.github.com>
2020-05-18 21:42:02 +0530

    Update 108-use-multiple-conditional-(ternary)-operators.js

--- basic_javaScript/108-use-multiple-conditional-(ternary)-operators.js
+++ basic_javaScript/108-use-multiple-conditional-(ternary)-operators.js
@@ -1,6 +1,4 @@
-/* Challenge Link:
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-multiple-conditional-ternary-operators
-*/
+
 
 /* =============== First Solution =============== */
 
